2fa430803adf083f54b0b705075e84556065b092
Sebastian F <sebpfave2@gmail.com>
2021-01-24 17:04:30 -0500

    refactoring

--- game.js
+++ game.js
@@ -7,42 +7,30 @@ let userPattern = [];
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
     return randomChosenColor;
 }
 
-function getButton(color) {
-    return $(`#${color}`);
-}
-
 function animateButton(color) {
-    flashButton(color);
-    playButtonSound(color);
-}
-
-function flashButton(color) {
-    let button = getButton(color);
+    let button = $(`#${color}`);
     button.fadeOut(125);
     button.fadeIn(125);
-}
 
-function playButtonSound(color) {
     let buttonSound = new Audio(`sounds/${color}.mp3`);
     buttonSound.play();
 }
 
-function clickedButton(button) {
-    userPattern.push(button.id);
-    console.log(userPattern);
-}
-
-
 // Main
-$("div .btn").click(function () {
-    clickedButton(this);
+$(".btn").click(function () {
+    let userSelectedColor = this.id;
+    userPattern.push(userSelectedColor);
+    console.log(userPattern);
 })
 
 let nextButton = nextSequence();
 animateButton(nextButton);
+
+
 
 // console.log(getButton(nextButton));
 // console.log(nextButton);
