f7558494782d9baf7eb82683704ce3d5e1d33e7e
Sebastian F <sebpfave2@gmail.com>
2021-01-24 17:22:27 -0500

    Animate user button click

--- game.js
+++ game.js
@@ -8,27 +8,50 @@ function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+    animateGameButton(randomChosenColor);
     return randomChosenColor;
 }
 
-function animateButton(color) {
-    let button = $(`#${color}`);
-    button.fadeOut(125);
-    button.fadeIn(125);
+function getButton(color) {
+    return $(`#${color}`);
+}
 
+function animateGameButton(color) {
+    flashButton(color);
+    playButtonSound(color);
+}
+
+function animateUserButton(color) {
+    shadowButton(color);
+    playButtonSound(color);
+}
+
+function flashButton(color) {
+    let button = getButton(color);
+    button.fadeOut(125).fadeIn(125);
+}
+
+function shadowButton(color) {
+    let button = getButton(color);
+    button.addClass("pressed");
+    setTimeout(function () { button.removeClass("pressed"); }, 125)
+}
+
+function playButtonSound(color) {
     let buttonSound = new Audio(`sounds/${color}.mp3`);
     buttonSound.play();
 }
 
+
 // Main
 $(".btn").click(function () {
     let userSelectedColor = this.id;
+    animateUserButton(userSelectedColor);
     userPattern.push(userSelectedColor);
-    console.log(userPattern);
+    // console.log(userPattern);
 })
 
 let nextButton = nextSequence();
-animateButton(nextButton);
 
 
 
